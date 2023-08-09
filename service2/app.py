5ac140573f5acd74f274aac51a2a88a5c7545f6e
sckelley4 <140641713+sckelley4@users.noreply.github.com>
2023-08-09 11:51:33 -0500

    Update app.py

--- service2/app.py
+++ service2/app.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello from Service 2!\n'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
