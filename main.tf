b5c950915faeac5d23191d64573708f953f777f1
sckelley4 <140641713+sckelley4@users.noreply.github.com>
2023-08-10 10:09:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,18 +2,18 @@
 provider "google" {
   credentials = file("/Users/sckelley/Downloads/devsecop-captsone-cf7a1a3762eb.json")
   project     = "devsecop-captsone"
-  region      = "us-south1"   # Replace with your desired region
+  region      = "us-south1"   # Replace with desired region
 }
 
 # Kubernetes Engine cluster
 resource "google_container_cluster" "my_cluster" {
   name               = "my-gke-cluster"
-  location           = "us-south1"   # Replace with your desired region
+  location           = "us-south1"   # Replace with desired region
   initial_node_count = 2               # Number of initial nodes in the cluster
 
   node_config {
-    machine_type = "n1-standard-1"     # Replace with your desired machine type
-    disk_size_gb = 30                  # Replace with your desired node disk size (in GB)
+    machine_type = "n1-standard-1"     # Replace with desired machine type
+    disk_size_gb = 30                  # Replace with desired node disk size (in GB)
   }
 
   # Optional: Enable network policy (if needed)
@@ -25,14 +25,14 @@ resource "google_container_cluster" "my_cluster" {
 # tags for the worker nodes
 resource "google_compute_instance" "cluster_worker_tags" {
   count        = google_container_cluster.my_cluster.initial_node_count
-  project      = "devsecop-captsone"    # Replace with your GCP project ID
+  project      = "devsecop-captsone"    # Replace with GCP project ID
   name         = "node-${count.index + 1}"  # Generate unique names for the instances
-  zone         = "nus-south1-a"          # Replace with your desired zone
-  machine_type = "n1-standard-1"          # Replace with your desired machine type
+  zone         = "nus-south1-a"          # Replace with desired zone
+  machine_type = "n1-standard-1"          # Replace with desired machine type
   tags         = ["web", "api"]           # Specify the desired tags for the worker nodes
 
   network_interface {
-    # You can specify the network and subnetwork if you want to use custom ones.
+    # specify the network and subnetwork if needed.
     # If not specified, it will use the default network and subnetwork.
   }
 
@@ -40,7 +40,7 @@ resource "google_compute_instance" "cluster_worker_tags" {
     auto_delete = true
     initialize_params {
       size = 30  # Replace with the desired size of the boot disk (in GB)
-      # You can specify other disk initialization parameters here if needed.
+      # specify other disk initialization parameters here if needed.
     }
   }
 }
